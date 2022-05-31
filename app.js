3f7ab2fb993f321f7b47225de1de6a77ca27aea4
anshul1610 <anshul.sharma@gupshup.io>
2022-05-31 10:08:03 +0530

    init

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post('/addtocart', (req, res) => {
         })
 });
 
-const port = 3000;
+const port = process.env.port || 3000;
 app.listen(port, (err) => {
     if (err) console.log(err);
     console.log("Server listening on port", port);
