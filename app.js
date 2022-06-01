01d910aba2534d3af21c4f78aec3253672357190
anshul1610 <anshul.sharma@gupshup.io>
2022-06-01 13:06:47 +0530

    init

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post('/cart', async (req, res) => {
     let payload = body.request.payload;
     let userId = req.body.user.id;
     payload = JSON.parse(JSON.stringify(payload));
-    console.log("payload-----", payload, payload.id, payload.signature, payload.payloadData);
+    console.log("payload-----", payload, payload['id'], payload['signature'], payload['payloadData']);
     console.log("payload2-----", payload.payloadData);
     console.log("payload3-----", payload.payloadData.data);
     console.log("payload4-----", Object.values(payload.payloadData.data))[0];
