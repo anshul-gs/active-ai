4e5f7d0530fd7081114428e7dcf16a00fc57fced
anshul1610 <anshul.sharma@gupshup.io>
2022-06-01 15:07:14 +0530

    init

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.post('/cart', async (req, res) => {
                     payload.payloadData.data["product.product"],
                     "20"
                 ]
-            }
+            };
             // let j = {
             //     "status": "success",
             //     "templateCode": "success",
@@ -96,9 +96,8 @@ app.post('/cart', async (req, res) => {
             //         "transaction_type": ""
             //     }
             // };
-            j = JSON.parse(JSON.stringify(j));
             console.log("res from db", j);
-            res.json(j);
+            res.send(j);
         })
         .catch((err) => {
             res.send(err);
