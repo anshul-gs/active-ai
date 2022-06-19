283b2f5fd482d4abcb0f67a84971d8f844d3c5a2
anshul1610 <anshul.sharma@gupshup.io>
2022-06-19 16:56:40 +0530

    init

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,7 @@ router.post('/callagent', async (req, res) => {
                 'x-api-key': 'GesxeTJGz52ReWg8UBb8w7fTtqaCy1107E6bNZmG'
             }
         }).then((response) => {
-            // console.log("call response", response.data);
+            console.log("call response", response.data);
             res.send(response.data);
         }).catch((err) => {
             // console.log("call err", err);
